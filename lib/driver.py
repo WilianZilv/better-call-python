173282742154a8321210bde7a294e6d07ada19d2
Wilian <wilianzilv@gmail.com>
2020-05-17 11:41:45 -0300

    added inline documentation

--- lib/driver.py
+++ lib/driver.py
@@ -19,29 +19,25 @@ def _____pprint_____(*args):
 print = _____pprint_____
 ''''''
 while True:
+
     try:
         line = sys.stdin.readline()
         data = json.loads(line)
     except:
         continue
+
     response = {}
     path = data['path']
     args = data['args']
 
     try:
-        typeof = str(type(eval(path)))
-        if 'function' in typeof:
+        if 'function' in str(type(eval(path))):
             path += '(**args)' if 'dict' in str(type(args)) else '(*args)'
 
-        exec('value='+path)
-
-        if 'ndarray' in str(type(value)):
-            value = {"data": value.tolist(), "type": "numpy.ndarray"}
-
+        exec('value=' + path)
         response['data'] = value
     except Exception as e:
         response["error"] = str(e)
 
-    sys.stdin.flush()
     sys.stdout.write(json.dumps(response) + '\n')
     sys.stdout.flush()
